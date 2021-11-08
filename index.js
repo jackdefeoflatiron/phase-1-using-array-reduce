e0edb1cb33c2caff888862305a9fb772abf36997
John DeFeo <jackd3243@gmail.com>
2021-11-08 10:59:27 -0500

    commit tree b6b7fb406e29bac7dcad0ea5f32007de09991418

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
-// Code your solution here
+
+
+// batteryBatches.reduce((a, b) => (a + b), 0 )
+let totalBatteries = batteryBatches.reduce((a, b) => (a + b), 0 )
+
+
